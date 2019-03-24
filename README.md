0ed663d0d73eb8425d93d0271440baed5fc00613
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2019-03-24 10:55:02 +0200

    Add Voccer and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,141 @@
-# voccer
+# Voccer
 Small Python3 based utility to monitor tVOC and eCO2 with Bosch BME680 and SGP30
+to MQTT server
+
+First of all read this:
+**Please note, this sensor, like all VOC/gas sensors, has variability and to
+get precise measurements you will want to calibrate it against known sources!**
+
+This means that you can monitor as much as you can but if you don't know your
+baseline then you can't get any good measurements. Project is bit HaX0ur so
+there is rough points that you need to fill and everyone is free to commit
+Pull Requests to full fill this README.md documentation.
+
+Currently Voccer is work in process and it can change every commit
+
+It's tested with these break-out boards
+
+ * [Adafruit SGP30 Air Quality Sensor Breakout - VOC and eCO2](https://www.adafruit.com/product/3709)
+ * [BME680 Breakout - Air Quality, Temperature, Pressure, Humidity Sensor BME680 Breakout - Air Quality, Temperature, Pressure, Humidity Sensor](https://shop.pimoroni.com/products/bme680-breakout)
+
+As noticed they both I2C based so one has to have skills to make them work
+with Raspberry Pi you need to have module *i2c-dev* loaded before using
+Python libraries. If you can see them in I2C-bus with *i2c-detect* then they
+should work.
+
+**As said earlier: it's up to you make sensors work**
+
+## Commanline args
+ | Arg | what                       | preset    |
+ |-----|----------------------------|-----------|
+ | -h  | Help                       |           |
+ | -s  | Sensor ID                  | 1         |
+ | -f  | Use second addr for BME680 | NO        |
+ | -t  | Temp offset for BME680     | 0         |
+ | -g  | Use SGP30                  | NO        |
+ | -m  | MQTT server address        | localhost |
+ | -p  | MQTT server port           | 1883      |
+
+## Getting started with BME680
+
+ * [Getting Started with BME680 Breakout](https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-bme680-breakout)
+ * [Pimoroni BME680 Github](https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-bme680-breakout)
+
+## Getting started with SGP30
+
+ * [Python library for reading co2 and TVOC from the Sensirion SGP30 ](https://pypi.org/project/sgp30/)
+
+## JSON
+JSON that script outputs to MQTT looks like this
+```
+{"timestamp": 1553413797.78, "id": 1, "value": 19.23, "type": "temperature", "typeid": 100, "unitid": 100}
+```
+
+## Types
+All those numbers in typeid and unitid are just numbers started from 100.
+ 
+ | ID  | Type        | Unit    |
+ |-----|-------------|---------|
+ | 100 | temperature | Celsius |
+ | 101 | humidity    | RH%     |
+ | 102 | pressure    | hPa     |
+ | 103 | AiQ         | AiQ     |
+ | 104 | Resistance  | Gas     |
+ | 105 | tVOC        | ppm     |
+ | 106 | eCO2        | ppm     |
+
+## Listening JSON from MQTT server
+and you can listen them with for example [Mosquitto MQTT-project](http://mosquitto.org/) tool *mosquitto_sub*
+```
+mosquitto_sub -t /sensor/voccer/2.0/+
+```
+
+# Setting up Fluentd
+There is several servers which can log these into database like [Fluentd](https://www.fluentd.org/) and
+[Telegraf](https://github.com/influxdata/telegraf) for example.
+If you choose to use Fluentd you need [Fluent plugin for MQTT Input/Output](https://github.com/toyokazu/fluent-plugin-mqtt-io) and if
+you like to log to database like MySQL or MariaDB then you need [SQL input/output plugin for Fluentd](https://github.com/fluent/fluent-plugin-sql)
+**As Raspbian doesn't support Ruby very you are mostly out of luck just use Pure Raspberry Pi solution**
+
+## MySQL table setup
+Project doen't currently provice SQL-statement to create table but it's like this
+
+| Field      | Type 	           | Null |  Key | Default | Extra          |
+|------------|---------------------|------|------|---------|----------------|
+| id         | bigint(20)          | NO   | PRI  | NULL    | auto_increment |
+| unitType   | tinyint(4) unsigned | NO   |      | 0       |                |	
+| sensorId   | tinyint(4) unsigned | NO   |      | 0 	   |                |
+| sensorType | tinyint(4) unsigned | NO   |      | 0 	   |                |
+| time 	     | int(11)             | NO   |      | 0 	   |                |
+| value 	 | double 	           | NO	  |      | 0 	   |                |
+
+## Fluentd setup
+There is good documentation for Fluentd configuration but this is simple config file
+to get Voccer logging working
+```
+# In v1 configuration, type and id are @ prefix parameters.
+# @type and @id are recommended. type and id are still available for backward compatibility
+
+<source>
+  @type mqtt
+  host 127.0.0.1
+  port 1883
+  topic /sensor/voccer/2.0/+
+  <parse>
+    @type json
+  </parse>
+</source>
+
+<match .sensor.voccer.2.0.**>
+  @type sql
+  host localhost
+  port 3306
+  database voccer
+  adapter mysql2
+  username voccer
+  password voccer
+  remove_tag_prefix .sensor.voccer.2.0
+
+  <table>
+    table measurement
+    column_mapping 'timestamp:time,unitid:unitType,id:sensorId,typeid:sensorType,value:value'
+  </table>
+</match>
+```
+
+# Grafana setup
+[Grafana](https://grafana.com/) is state of art visualition tool and suits to this purpose very well
+after installing and creating dashboard with MySQL connector one can create SQL query statement for Panel
+like this
+```
+SELECT
+  time AS "time",
+  value AS value
+FROM measurement
+WHERE
+  $__unixEpochFilter(time) AND
+  sensorType = 100 AND
+  sensorId = 1
+ORDER BY time
+```
+And it will start drawing temperature panel for sensor 1.
---
+++ voccer.py
@@ -0,0 +1,336 @@
+""" VOCCER is simple Python 3.x script which
+    Tries to read from diffrent sensors
+    and forward them to MQTT-server which """
+
+# Copyright (c) 2019 Tuukka Pasanen
+# SPDX-License-Identifier: MIT
+
+# pylint: disable=E0401
+import json
+import time
+import sys
+import getopt
+import bme680
+import paho.mqtt.client as mqtt
+from smbus2 import SMBusWrapper
+from sgp30 import Sgp30
+
+
+def on_connect(mqttc, obj, flags, rtn_int):
+    """Paho Connection callback."""
+    # pylint: disable=W0612,W0613
+    print("Connected: " + str(rtn_int))
+
+
+def on_message(mqttc, obj, msg):
+    """Paho Message send callback."""
+    # pylint: disable=W0612,W0613
+    print("Message:" + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+def on_publish(mqttc, obj, mid):
+    """Paho Message published callback."""
+    # pylint: disable=W0612,W0613
+    print("Published: " + str(mid))
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    """Paho Server subscribe callback."""
+    # pylint: disable=W0612,W0613
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mqttc, obj, level, string):
+    """Paho logging callback."""
+    # pylint: disable=W0612,W0613
+    print("Log: " + string)
+
+
+def measure_hash(sensor_id, measure_value, measure_type):
+    """Create measurement JSON"""
+    typeid = 100
+    unitid = 100
+    if measure_type == "voc":
+        typeid = 105
+        unitid = 105
+    elif measure_type == "temperature":
+        typeid = 100
+        unitid = 100
+    elif measure_type == "humidity":
+        typeid = 101
+        unitid = 101
+    elif measure_type == "pressure":
+        typeid = 102
+        unitid = 102
+    elif measure_type == "quality":
+        typeid = 103
+        unitid = 103
+    elif measure_type == "gas":
+        typeid = 104
+        unitid = 104
+    elif measure_type == "co2":
+        typeid = 106
+        unitid = 106
+
+    return json.dumps({
+        'timestamp': round(time.time(), 2),
+        'id': int(sensor_id),
+        'value': measure_value,
+        'type': measure_type,
+        'typeid': typeid,
+        'unitid': unitid
+    })
+
+
+def air_quality_score(sensor, gas_baseline, hum_baseline):
+    """Calculate air quality score from BME680 or else"""
+    # This sets the balance between humidity and gas reading in the
+    # calculation of air_quality_score (25:75, humidity:gas)
+    hum_weighting = 0.25
+
+    sensor.get_sensor_data()
+
+    if sensor.get_sensor_data() and sensor.data.heat_stable:
+        gas = sensor.data.gas_resistance
+        gas_offset = gas_baseline - gas
+        hum = sensor.data.humidity
+        hum_offset = hum - hum_baseline
+
+        # Calculate hum_score as the distance from the hum_baseline.
+        if hum_offset > 0:
+            hum_score = (100 - hum_baseline - hum_offset)
+            hum_score /= (100 - hum_baseline)
+            hum_score *= (hum_weighting * 100)
+        else:
+            hum_score = (hum_baseline + hum_offset)
+            hum_score /= hum_baseline
+            hum_score *= (hum_weighting * 100)
+
+        # Calculate gas_score as the distance from the gas_baseline.
+        if gas_offset > 0:
+            gas_score = (gas / gas_baseline)
+            gas_score *= (100 - (hum_weighting * 100))
+        else:
+            gas_score = 100 - (hum_weighting * 100)
+
+        # Calculate air_quality_score.
+        return hum_score + gas_score
+
+    return 0
+
+
+def get_bme680_air_baseline(sensor):
+    """ Calculate Gas baseline """
+    start_time = time.time()
+    curr_time = time.time()
+    burn_in_time = 5 * 60
+
+    burn_in_data = []
+
+    try:
+        # Collect gas resistance burn-in values, then use the average
+        # of the last 50 values to set the upper limit for calculating
+        # gas_baseline.
+        print('Collecting gas resistance burn-in data for 5 mins\n')
+        while curr_time - start_time < burn_in_time:
+            left_time = int(burn_in_time - (curr_time - start_time))
+            curr_time = time.time()
+            if sensor.get_sensor_data() and sensor.data.heat_stable:
+                gas = sensor.data.gas_resistance
+                burn_in_data.append(gas)
+                print('Gas {0}: {1} Ohms'.format(left_time, gas))
+                time.sleep(2)
+
+        gas_baseline = sum(burn_in_data[-50:]) / 50.0
+    except KeyboardInterrupt:
+        pass
+    return gas_baseline
+
+
+def set_bme680_config(sensor, temp_offset):
+    """ Function to set confis for BOSCH BME680
+        These oversampling settings can be tweaked to
+        change the balance between accuracy and noise in
+        the data """
+    sensor.set_humidity_oversample(bme680.OS_2X)
+    sensor.set_pressure_oversample(bme680.OS_4X)
+    sensor.set_temperature_oversample(bme680.OS_8X)
+    sensor.set_filter(bme680.FILTER_SIZE_3)
+    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+    sensor.set_temp_offset(temp_offset)
+
+    print('\n\nInitial reading:')
+    for name in dir(sensor.data):
+        value = getattr(sensor.data, name)
+
+        if not name.startswith('_'):
+            print('{}: {}'.format(name, value))
+
+    sensor.set_gas_heater_temperature(320)
+    sensor.set_gas_heater_duration(150)
+    sensor.select_gas_heater_profile(0)
+
+    print('Calibration data:')
+    for name in dir(sensor.calibration_data):
+
+        if not name.startswith('_'):
+            value = getattr(sensor.calibration_data, name)
+
+            if isinstance(value, int):
+                print('{}: {}'.format(name, value))
+
+
+def mainloop_bme680(sensor, sensor_id, mqttc, mqtt_topic, gas_baseline):
+    """ Main loop to run Bosch BME680 sensor """
+    # pylint: disable=W0612,W0613
+
+    # Set the humidity baseline to 40%, an optimal indoor humidity.
+    hum_baseline = 40.0
+
+    print('Gas baseline: {0} Ohms, humidity baseline: {1:.2f} %RH\n'.format(
+        gas_baseline, hum_baseline))
+
+    while True:
+        if sensor.get_sensor_data():
+            temperature = measure_hash(sensor_id,
+                                       round(sensor.data.temperature, 2),
+                                       "temperature")
+            pressure = measure_hash(sensor_id, int(sensor.data.pressure),
+                                    "pressure")
+            humidity = measure_hash(sensor_id, int(sensor.data.humidity),
+                                    "humidity")
+            gas = measure_hash(sensor_id, round(sensor.data.gas_resistance, 2),
+                               "gas")
+
+            air_quality_scr = air_quality_score(sensor, gas_baseline,
+                                                hum_baseline)
+            air_quality = measure_hash(sensor_id, round(air_quality_scr, 2),
+                                       "quality")
+
+            print(
+                'Temperature: {0:.2f}C, Pressure: {1:.2f} HPa, Humidity {2:.2f} '
+                .format(sensor.data.temperature, sensor.data.pressure,
+                        sensor.data.humidity))
+            print(
+                '%RH, Resistance: {3:.2f} Ohm, Quality Indx {4:.2f}\n'.format(
+                    sensor.data.gas_resistance, air_quality_scr))
+
+            (rtn_value, mid) = mqttc.publish(
+                mqtt_topic + "temperature", temperature, qos=0)
+
+            (rtn_value, mid) = mqttc.publish(
+                mqtt_topic + "pressure", pressure, qos=0)
+
+            (rtn_value, mid) = mqttc.publish(
+                mqtt_topic + "humidity", humidity, qos=0)
+
+            (rtn_value, mid) = mqttc.publish(mqtt_topic + "gas", gas, qos=0)
+
+            (rtn_value, mid) = mqttc.publish(
+                mqtt_topic + "quality", air_quality, qos=0)
+
+        time.sleep(10 * 60)
+
+
+def sgp30_mainloop(sensor_id, mqttc, mqtt_topic):
+    """ Main loop to run SGP30 sensor """
+    # pylint: disable=W0612,W0613
+    with SMBusWrapper(1) as bus:
+        sgp = Sgp30(
+            bus, baseline_filename="/tmp/mySGP30_baseline"
+        )  #things thing with the baselinefile is dumb and will be changed in the future
+        print("resetting all i2c devices")
+
+        sgp.i2c_geral_call(
+        )  #WARNING: Will reset any device on teh i2cbus that listens for general call
+
+        print(sgp.read_features())
+        print(sgp.read_serial())
+        sgp.init_sgp()
+
+        while True:
+            sgp_measurements = sgp.read_measurements()
+
+            co2 = measure_hash(sensor_id, int(sgp_measurements.data[0]), "co2")
+            tvoc = measure_hash(sensor_id, int(sgp_measurements.data[1]),
+                                "voc")
+
+            (rtn_value, mid) = mqttc.publish(mqtt_topic + "co2", co2, qos=0)
+            (rtn_value, mid) = mqttc.publish(mqtt_topic + "tvoc", tvoc, qos=0)
+
+            print("eCO2: " + str(sgp_measurements.data[0]) + " tVOC: " +
+                  str(sgp_measurements.data[1]))
+            time.sleep(10 * 60)
+
+
+def main(argv):
+    """ Main function """
+    # pylint: disable=W0612,W0613,R0912
+    sensor_id = 1
+    bme680_addr = bme680.I2C_ADDR_PRIMARY
+    mqtt_server = "localhost"
+    mqtt_port = 1883
+    sgp_sensor = False
+    temp_offset = 0
+
+    try:
+        opts, args = getopt.getopt(argv, "hs:fm:p:gt:", [
+            "sensorid=", "bme680second", "mqttserver", "mqttport", "sgp30",
+            "tempoffset"
+        ])
+    except getopt.GetoptError:
+        print('voccer.py -s sensor_id -f Bosch BME680 secondary addr')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('voccer.py -s sensor_id -f Bosch BME680 secondary addr')
+            sys.exit()
+        elif opt in ("-s", "--sensorid"):
+            sensor_id = arg
+        elif opt in ("-f", "--bme680second"):
+            bme680_addr = bme680.I2C_ADDR_SECONDARY
+        elif opt in ("-g", "--sgp30"):
+            sgp_sensor = True
+        elif opt in ("-m", "--mqttserver"):
+            mqtt_server = arg
+        elif opt in ("-p", "--mqttport"):
+            mqtt_port = arg
+        elif opt in ("-t", "--tempoffset"):
+            temp_offset = arg
+
+    mqttc = mqtt.Client()
+
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    # mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+
+    # Uncomment to enable debug messages
+    # mqttc.on_log = on_log
+    mqttc.loop_start()
+    mqttc.connect(mqtt_server, mqtt_port, 60)
+    mqtt_topic = "/sensor/voccer/2.0/"
+
+    if sgp_sensor is False:
+        try:
+            sensor = bme680.BME680(bme680_addr)
+        except IOError:
+            print("Can't open BME680 at I2C addr" + bme680_addr)
+            sys.exit(2)
+    else:
+        sgp30_mainloop(sensor_id, mqttc, mqtt_topic)
+        sys.exit(0)
+
+    set_bme680_config(sensor, temp_offset)
+
+    try:
+        gas_baseline = get_bme680_air_baseline(sensor)
+
+        mainloop_bme680(sensor, sensor_id, mqttc, mqtt_topic, gas_baseline)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
