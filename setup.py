5faaeca7a570dcb302b3e1c30f45e8d197646b1d
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2020-05-28 13:24:48 +0300

    Add install_requires and requirements.txt to project in order to get this in pypi.org somepoint of time

--- setup.py
+++ setup.py
@@ -10,12 +10,22 @@ setuptools.setup(
     version="0.0.1",
     author="Tuukka Pasanen",
     author_email="tuukka.pasanen@ilmi.fi",
-    description=("Small Python3 based utility to send tVOC, eCO2 and Particulate Matter to MQTT server"),
+    description=
+    ("Small Python3 based utility to send tVOC, eCO2 and Particulate Matter to MQTT server"
+     ),
     license="Propiertary",
     keywords="tVOC VOC eCO2 CO2 Matter",
     url="https://github.com/illuusio/voccer",
     scripts=['voccer.py'],
     packages=setuptools.find_packages(),
+    install_requires=[
+        'paho.mqtt.client>=1.0.0'
+        'RPi.GPIO>=0.6.0'
+        'bme680>=1.0.0'
+        'smbus2>=0.3.0'
+        'sgp30>=0.1.6'
+        'pms5003>=0.0.5'
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
@@ -23,4 +33,3 @@ setuptools.setup(
         "Topic :: Utilities",
     ],
 )
-
