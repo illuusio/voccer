3538ba08c3efd9eec54bdac738c82ef38469e3d1
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2020-05-28 12:57:25 +0300

    Add setup.py to have more python like experience

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="voccer",
+    version="0.0.1",
+    author="Tuukka Pasanen",
+    author_email="tuukka.pasanen@ilmi.fi",
+    description=("Small Python3 based utility to send tVOC, eCO2 and Particulate Matter to MQTT server"),
+    license="Propiertary",
+    keywords="tVOC VOC eCO2 CO2 Matter",
+    url="https://github.com/illuusio/voccer",
+    scripts=['voccer.py'],
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 2 - Beta",
+        "Topic :: Utilities",
+    ],
+)
+
